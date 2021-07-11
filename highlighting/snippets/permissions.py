88e9c2e59672292603a60d23f7237c403a914af3
Charlie Cumming <charliejc0806@gmail.com>
2021-07-11 19:13:07 +1200

    Tutorial part 5. Implement relationships and hyperlinks Tutorial Part 6 Implement viewsets and routers

--- highlighting/snippets/permissions.py
+++ highlighting/snippets/permissions.py
@@ -7,6 +7,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
-            return True;
+            return True
 
         return obj.owner == request.user
